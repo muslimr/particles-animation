ceb82a279e0589762c686f23d4a18f9f8a162c51
muslimragimov <muslim.ragimov@yahoo.com>
2022-12-21 00:28:08 +0400

    - created react project

---
+++ src/components/animated-background/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function AnimatedBackground() {
+    return(
+        <div>
+            Animated Background
+
+        </div>
+    )
+}
---
+++ src/layouts/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+import AnimatedBackground from "../components/animated-background";
+
+
+function App() {
+    return (
+        <div className="App">
+            <AnimatedBackground />
+        </div>
+    );
+}
+
+export default App;
